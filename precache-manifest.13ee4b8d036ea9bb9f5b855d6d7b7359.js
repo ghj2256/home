bdc185ac84da0e0cd9a23843396596d9f2514a7e
ghj2256 <ghj2256@gmail.com>
2021-11-15 01:00:22 +0900

    Updates

---
+++ precache-manifest.13ee4b8d036ea9bb9f5b855d6d7b7359.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cdca9a71e329002913b69f320c9169d",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "fa8f718f9863b576a674",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "9f40b02a1668d0eed82f",
+    "url": "/home/static/js/2.33463950.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.33463950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa8f718f9863b576a674",
+    "url": "/home/static/js/main.419d7b0b.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "c411af98cd0fe7bb08be47ac3d77c691",
+    "url": "/home/static/media/IMG_7569.c411af98.JPG"
+  },
+  {
+    "revision": "a39990837869d6c988b485f814f9a3b9",
+    "url": "/home/static/media/ghj2256.a3999083.pdf"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/home/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/home/static/media/resume.9d3da0e2.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.df84cd9571af5c9c3b4e97876ee49e51.js"
+  "/home/precache-manifest.13ee4b8d036ea9bb9f5b855d6d7b7359.js"
 );
 
 self.addEventListener('message', (event) => {
